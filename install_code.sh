8bd9ec1d65a0677affe849e8e537881aa4f6360a
Lion A <ackerm.lion@hotmail.de>
2021-11-19 11:09:28 +0100

    Add install script for VS Code

---
+++ install_code.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+cp vscode/settings.json $HOME/.config/Code/User/keybindings.json
+cp vscode/snippets/python.json $HOME/.config/Code/User/snippets/python.json
+
