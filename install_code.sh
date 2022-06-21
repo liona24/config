6f6da4bde55c60bdbb072a1f93a11b7436a9e671
lion <ackerm.lion@hotmail.de>
2022-06-21 17:31:49 +0200

    Fix code installation

--- install_code.sh
+++ install_code.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cp vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
-cp vscode/settings.json $HOME/.config/Code/User/keybindings.json
+cp vscode/settings.json $HOME/.config/Code/User/settings.json
+
+(mkdir -p $HOME/.config/Code/User/snippets || :)
 cp vscode/snippets/python.json $HOME/.config/Code/User/snippets/python.json
 
