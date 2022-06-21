b19e544054e7f04b93431197ac0ab0dc393955c5
lion <ackerm.lion@hotmail.de>
2022-06-21 17:59:43 +0200

    Install config as symlink

--- install_code.sh
+++ install_code.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
-cp vscode/settings.json $HOME/.config/Code/User/settings.json
+ln -s -f $(pwd)/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
+ln -s -f $(pwd)/vscode/settings.json $HOME/.config/Code/User/settings.json
 
 (mkdir -p $HOME/.config/Code/User/snippets || :)
-cp vscode/snippets/python.json $HOME/.config/Code/User/snippets/python.json
+ln -s -f $(pwd)/vscode/snippets/python.json $HOME/.config/Code/User/snippets/python.json
 
